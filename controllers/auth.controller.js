1acbca6aa029f8a923f86a0a3e4040e627b33d4d
ctabares <camilo.tabares@uruit.com>
2021-09-09 15:20:41 -0500

    session with cookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,9 @@ const { loginService } = require('../services/auth.service');
 const loginController = (req, res, next) => {
   const { email, password } = req.body;
   return loginService(email, password)
-    .then(user => res.send(user));
+    .then(user => res.cookie("auth_pok", "want some cookies?", {
+      expires: new Date(Date.now() + 900000), httpOnly: true
+    }).send(user));
 }
  
 module.exports = {

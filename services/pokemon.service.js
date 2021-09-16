4712c41862fe1cf8d23b11d3dae7195dc4d3fc8c
ctabares <camilo.tabares@uruit.com>
2021-09-16 18:28:39 -0500

    refactor to getfullgenerations service

--- services/pokemon.service.js
+++ services/pokemon.service.js
@@ -1,15 +1,11 @@
 const { fetch } = require('../utils');
-
-const groupVersions = versions => versions.reduce((acc, version) => {
-  acc.push(...version);
-  return acc;
-}, []);
+const { ServerInternalError } = require('../errors');
 
 const getGenerationsService = () =>
   fetch.get('/generation')
     .then(response => {
       if (!response.status) {
-        throw new Error('An error ocurred while executing the request');
+        throw new ServerInternalError('An error ocurred while executing the request');
       }
 
       return response.data
@@ -19,7 +15,7 @@ const getGenerationByName = (name) =>
   fetch.get(`generation/${name}`)
     .then((response => {
       if (!response.status) {
-        throw new Error('An error ocurred while executing the request');
+        throw new ServerInternalError('An error ocurred while executing the request');
       }
 
       return response.data;
@@ -29,37 +25,49 @@ const getVersionGroupByName = (name) =>
   fetch.get(`version-group/${name}`)
     .then((response => {
       if (!response.status) {
-        throw new Error('An error ocurred while executing the request');
+        throw new ServerInternalError('An error ocurred while executing the request');
       }
 
       return response.data;
     }));
 
-
 const getFullGenerations = () =>
   getGenerationsService()
-    .then(generations => generations.results.map(({ name }) => getGenerationByName(name)))
-    .then(responses => Promise.all(responses))
-    .then(generations => generations.map(({
-      version_groups,
-      name,
-      main_region,
-      id
-    }) =>
-      Promise.all(version_groups.map(({ name: version_name }) =>
-        getVersionGroupByName(version_name)
-          .then(({ versions }) => versions)
+    .then(generations => generations.results.map(({ name }) =>
+      getGenerationByName(name).then(({ version_groups, main_region, id }) =>
+        Promise.all(version_groups.map(({ name: version_name }) =>
+          getVersionGroupByName(version_name)
+            .then(({ versions }) => versions.flat())
+        ))
+          .then(games => ({
+            id,
+            name,
+            main_region,
+            games,
+          }))
       ))
-        .then(groupVersions)
-        .then(games => ({
-          id,
-          name,
-          main_region,
-          games,
-        }))
-    ))
+    )
     .then(result => Promise.all(result))
 
+const getFullGenerationsAsync = async () => {
+    const { results: allGenerations } = await getGenerationsService();
+    const generationsWithGames = allGenerations.map(async ({ name }) => {
+      const { version_groups, main_region, id } = await getGenerationByName(name);
+      const gamesArray = version_groups.map(({ name: version_name }) => getVersionGroupByName(version_name));
+      const games = await Promise.all(gamesArray);
+
+      return {
+        id,
+        name,
+        main_region,
+        games,
+      }
+    });
+
+    return Promise.all(generationsWithGames);
+}
+
 module.exports = {
-  getFullGenerations,
+  getFullGenerations: getFullGenerationsAsync,
+  // getFullGenerations
 }
